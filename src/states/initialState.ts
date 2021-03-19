760b21e9142b06e6b48a000522ec70c1c5a6bb3a
Kazushi Isogai <kazu150@gmail.com>
2021-03-19 11:10:21 +0900

    state.chatsの初期値変更、stateの型定義修正

--- src/states/initialState.ts
+++ src/states/initialState.ts
@@ -1,51 +1,37 @@
-const initialState = {
+export type State = {
+  user: {
+    id: string;
+    name: string;
+    email: string;
+    thumb: string;
+  };
+  chats: {
+    id: number;
+    name: string;
+    thumb: string;
+    createdAt: string;
+    description: string;
+  }[];
+  error: {
+    isOpened: boolean;
+    message: string;
+    errorPart: string;
+  };
+};
+
+const initialState: State = {
   user: {
     id: '',
     name: '',
     email: '',
     thumb: 'avatar.png',
   },
-  chats: [
-    {
-      id: 1,
-      name: 'クロネコ太郎',
-      thumb: 'avatar.png',
-      createdAt: '2021/03/17 21:20',
-      description:
-        '本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文',
-    },
-    {
-      id: 2,
-      name: 'シロネコ次郎',
-      thumb: 'avatar.png',
-      createdAt: '2021/03/17 21:30',
-      description:
-        '本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文',
-    },
-    {
-      id: 3,
-      name: 'ミケネコ三郎',
-      thumb: 'avatar.png',
-      createdAt: '2021/03/17 21:40',
-      description:
-        '本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文',
-    },
-    {
-      id: 4,
-      name: 'チャトラ四郎',
-      thumb: 'avatar.png',
-      createdAt: '2021/03/17 21:50',
-      description:
-        '本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文',
-    },
-  ],
+  chats: [],
   error: {
     isOpened: false,
     message: '',
     errorPart: '',
   },
 };
-
-export type State = typeof initialState;
 
 export default initialState;
