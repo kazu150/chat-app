135e1a049208dc6d818d00aaced18ab79b4c805d
Kazushi Isogai <kazu150@gmail.com>
2021-03-20 16:04:40 +0900

    時間取得ロジックの修正

--- src/firebase/fetchChats.ts
+++ src/firebase/fetchChats.ts
@@ -28,19 +28,13 @@ const fetchChats = (
           (user) => user.id === doc.data().publicProfiles.id
         )[0];
         const date: Date = doc.data().createdAt?.toDate();
-        formatDate(date);
-        const y = date?.getFullYear();
-        const m = `00${date?.getMonth() + 1}`.slice(-2);
-        const d = `00${date?.getDate()}`.slice(-2);
-        const h = `00${date?.getHours()}`.slice(-2);
-        const min = `00${date?.getMinutes()}`.slice(-2);
-        const formedDate = date ? `${y}/${m}/${d} ${h}:${min}` : '　';
+        const formattedDate = formatDate(date);
 
         return {
           id: Number(doc.id),
           name: filteredUser?.name,
           thumb: filteredUser?.thumb,
-          createdAt: formedDate,
+          createdAt: formattedDate,
           description: doc.data().description as string,
         };
       });
--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,11 +1,12 @@
 const formatDate = (date: Date | null): string => {
-  const y = date?.getFullYear();
-  const m = `00${date?.getMonth() + 1}`.slice(-2);
-  const d = `00${date?.getDate()}`.slice(-2);
-  const h = `00${date?.getHours()}`.slice(-2);
-  const min = `00${date?.getMinutes()}`.slice(-2);
+  if (!date) return '____/__/__ __:__';
+  const y = date.getFullYear();
+  const m = `00${date.getMonth() + 1}`.slice(-2);
+  const d = `00${date.getDate()}`.slice(-2);
+  const h = `00${date.getHours()}`.slice(-2);
+  const min = `00${date.getMinutes()}`.slice(-2);
 
-  return `${y}/${m}/${d} ${h}:${min}` || '　';
+  return `${y}/${m}/${d} ${h}:${min}`;
 };
 
 export default formatDate;
